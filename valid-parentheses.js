f356a4ad04435b7c325f2ab5c3e8f02e861e3a79
Elena Mudrakova <meishal7@gmail.com>
2022-03-18 11:26:33 -0700

    .

---
+++ valid-parentheses.js
@@ -0,0 +1,18 @@
+function validParentheses(parens) {
+    let arr = [];
+    for(let i = 0; i< parens.length; i++){
+      if(parens[i] == "(") arr.push(parens[i]);
+      else{
+        if(arr.length == 0) return false;
+        else{
+          
+          if(arr[0] == "("){
+            arr.shift();
+          }
+        }
+      }
+    }
+    if(arr.length == 0) return true;
+    else return false;
+  
+  }
