3a552550927cd9879e9642bc3d3db5ac3764d0b3
Elena Mudrakova <meishal7@gmail.com>
2022-03-25 11:13:29 -0700

    .

--- 5/valid-parentheses.js
+++ 5/valid-parentheses.js
@@ -1,18 +1,16 @@
 function validParentheses(parens) {
-    let arr = [];
-    for(let i = 0; i< parens.length; i++){
-      if(parens[i] == "(") arr.push(parens[i]);
-      else{
-        if(arr.length == 0) return false;
-        else{
-          
-          if(arr[0] == "("){
-            arr.shift();
-          }
+  let arr = [];
+  for (let i = 0; i < parens.length; i++) {
+    if (parens[i] == "(") arr.push(parens[i]);
+    else {
+      if (arr.length == 0) return false;
+      else {
+        if (arr[0] == "(") {
+          arr.shift();
         }
       }
     }
-    if(arr.length == 0) return true;
-    else return false;
-  
   }
+  if (arr.length == 0) return true;
+  else return false;
+}
--- 6/traverse-array-element-diagonally.js
+++ 6/traverse-array-element-diagonally.js
@@ -1,27 +1,26 @@
 function diagonal(arr) {
-    let rows = arr.length;
-    let columns = arr[0].length;
-    let diagonals = rows + columns-1;
-    let numofsubarrays = diagonals;
-    // create arr with length == numofsubarrays
-    let newarr = new Array(numofsubarrays);
-                    
-    // create empty subarrays
-    for(let i = 0; i < numofsubarrays; i++ ){
-            newarr[i] = [];
+  let rows = arr.length;
+  let columns = arr[0].length;
+  let diagonals = rows + columns - 1;
+  let numofsubarrays = diagonals;
+  // create arr with length == numofsubarrays
+  let newarr = new Array(numofsubarrays);
+
+  // create empty subarrays
+  for (let i = 0; i < numofsubarrays; i++) {
+    newarr[i] = [];
+  }
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    for (let j = 0; j <= arr[i].length - 1; j++) {
+      let subArrayIndex = i + j;
+      newarr[subArrayIndex].push(arr[i][j]);
     }
+  }
 
-    for(let i =0; i<=arr.length-1; i++){
-        for(let j =0; j<=arr[i].length-1; j++){
-            let subArrayIndex = i+j;
-            newarr[subArrayIndex].push(arr[i][j]);
-            }
-    }             
-                    
-    let reversed = newarr.reverse();
-    // create one array from array of arrays             
-    let oneArr = reversed.reduce((acc, value) => ([...acc, ...value]), []);
-    
-    return oneArr;
+  let reversed = newarr.reverse();
+  // create one array from array of arrays
+  let oneArr = reversed.reduce((acc, value) => [...acc, ...value], []);
 
+  return oneArr;
 }
