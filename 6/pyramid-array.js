5ca344f9b4382e466ea1e7f22602ab0e2205e4a0
Elena Mudrakova <meishal7@gmail.com>
2022-03-25 11:11:18 -0700

    .

---
+++ 6/pyramid-array.js
@@ -0,0 +1,19 @@
+function pyramid(n) {
+            let arr = [];
+            if(n == 0) {return arr};
+            let counter = 1;
+            while(counter < ( n+1)){
+                
+                let subarray = [];
+                for(let i =0; i<counter; i++){
+                    subarray.push(1);
+                }
+                
+                arr.push(subarray);
+                counter++;
+            }
+
+            console.log(arr);
+            return arr;
+ 
+}
